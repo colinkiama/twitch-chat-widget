40afc682e5dfbb16fefefec5d44b7f5adfdc2e9c
Colin Kiama <colinkiama@gmail.com>
2021-01-01 18:48:46 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const tmi = require('tmi.js');
+
+const client = new tmi.Client({
+	connection: {
+		secure: true,
+		reconnect: true
+	},
+	channels: [ 'my_name' ]
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+	// "Alca: Hello, World!"
+	console.log(`${tags['display-name']}: ${message}`);
+});
+				
